0829ec493264d93d93c9e714fce4cd93b3d2c7e4
cdROma <mrv24ru@yandex.ru>
2020-01-03 02:35:09 +0300

    Create .eslintrc.js
    eslintrc added

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'extends': 'airbnb',
+  'parser': 'babel-eslint',
+  'env': {
+    'jest': true,
+  },
+  'rules': {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off',
+    'no-unused-expressions': 'off',
+    'no-underscore-dangle': 'off',
+    'no-plusplus': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off'
+  },
+  'globals': {
+    "fetch": false
+  }
+}
